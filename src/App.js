07d6e7194a7ebce9fb410df8eb5759242249b519
skolkma <skolkma1@gmail.com>
2022-08-22 16:04:39 +0200

    mapping over pokemon array, render <Pokemon .../> component instance for each item, passing in the right props

--- src/App.js
+++ src/App.js
@@ -3,39 +3,52 @@ import "./App.css";
 import Title from "./components/Title";
 import Pokemon from "./components/Pokemon";
 
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <main>
         <Title content="Some Simple Title" />
-        <Pokemon
-          name="Charizard"
-          weight={90}
-          awesome={true}
-          terrifying={false}
-          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
-        />
-        <Pokemon
-          name="Bulbasaur"
-          weight={6.9}
-          awesome={true}
-          terrifying={false}
-          abilities={["Overgrow", "Chlorophyll"]}
-        />
-        <Pokemon
-          name="Mewtwo"
-          weight={122}
-          awesome={true}
-          terrifying={true}
-          abilities={["Pressure", "Unnerve", "Steadfast", "Insomnia"]}
-        />
-        <Pokemon
-          name="Mega beedrill"
-          weight={65}
-          awesome={false}
-          terrifying={true}
-          abilities={["Intimidate", "Unnerve"]}
-        />
+        {all_pokemon.map((pokemon, index) => (
+          <Pokemon
+            key={index}
+            name={pokemon.name}
+            weight={pokemon.weight}
+            awesome={pokemon.awesome}
+            terrifying={pokemon.terrifying}
+            abilities={pokemon.abilities}
+          />
+        ))}
       </main>
 
       <header className="App-header">
