c1a2e17e9200e7a788aa7b02540fe33db3e8943e
skolkma <skolkma1@gmail.com>
2022-08-22 13:59:49 +0200

    Component in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        <h1>Hello world</h1>
-        <p>React is cool!</p>
-        <footer>React App © Codaisseur Bootcamp</footer>
-      </p>
+      <main>
+        <Title />
+      </main>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,3 @@
+export default function Title() {
+  return <h1>Hi there! Welcome to React.</h1>;
+}
